b8727f2a5e0aff3b18e6b92f47a72c71ddba7cbd
SantaCruz77 <steveyamada66@gmail.com>
2023-06-12 08:00:34 +0900

    APIから気象データを取得して表示する。

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,35 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Title />
+  <Form @submit-form="getWeather" />
+  <Results />
+  {{ results }}
 </template>
+<script setup>
+import { reactive } from "vue";
+import axios from "axios";
+import Title from "./components/Title.vue";
+import Form from "./components/Form.vue";
+import Results from "./components/Results.vue";
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+const results = reactive({
+  country: "",
+  cityName: "",
+  temperature: "",
+  conditionText: "",
+  icon: "",
+});
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+const getWeather = () => {
+  axios
+    .get(
+      "https://api.weatherapi.com/v1/current.json?key=1ebb115188054f369fa11205232103&q=London&aqi=no"
+    )
+    .then((res) => {
+      (results.country = res.data.location.country),
+        (results.cityName = res.data.location.name),
+        (results.temperature = res.data.current.temp_c),
+        (results.conditionText = res.data.current.condition.text),
+        (results.icon = res.data.current.condition.icon);
+    });
+};
+</script>
